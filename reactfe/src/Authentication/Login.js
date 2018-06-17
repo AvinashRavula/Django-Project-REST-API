681e84fe7ce9c7ba481d917fea33c168ed816bc5
AvinashRavula <avinashravula@yahoo.com>
2018-06-17 15:46:03 +0530

    Added Django template integration

--- reactfe/src/Authentication/Login.js
+++ reactfe/src/Authentication/Login.js
@@ -64,7 +64,7 @@ class Login extends Component{
                 this.props.updateUsername(formData.get('username'));
                 this.props.updateStatus(true);
                 this.setState(prev => ( {buttonName : 'Logout'}));
-                this.props.history.push('/');
+                this.props.history.push('/onlineapp/templateview/');
                 console.log("Redirecting....")
             }
             else{
